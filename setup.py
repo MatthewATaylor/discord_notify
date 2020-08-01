28b6591b21542afa9016cb15890abdf489f602d7
Matthew Taylor <MatthewAlanTaylor@outlook.com>
2020-08-01 19:07:02 -0400

    Add long description

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup
 
+with open("README.md") as file:
+    long_description = file.read()
+
 setup(
     name="discord_notify",
     version="0.0.1",
     description="A Discord bot wrapper for easily sending messages to a channel",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/MatthewATaylor/discord_notify",
     author="Matthew Taylor",
     author_email="matthewalantaylor2@gmail.com",
     license="MIT",
-    packages=["discord_notify"],
-    zip_safe=False
+    packages=["discord_notify"]
 )
